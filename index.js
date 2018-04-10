349eb1d0a1d2b0a02a4d6fde84573e99795aecff
James Lee <lee1870@purdue.edu>
2018-04-09 23:26:51 -0400

    Added core database setup

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3(':memory:');
+//var database = require('sqlite3.js');
+//var db = openDatabase('mydb', '4.0.0', 'my first database', 2 * 1024 * 1024);
+//create connection to database
+//if the database is NOT found, create one
+var db = new sqlite3.Database('Game-Plan users');
+db.serialize(function() {
+    db.run("CREATE TABLE user (username TEXT, password TEXT, level INT)");
 
+});
+/*
+function createDb(){
+    console.log("creating Database
+
+}*/
 app.get('/', function(req, res){
-        res.send('<h1>Hello world<h1>');
+    res.send('<h1>Hello world<h1>');
 });
 
 http.listen(6969, function() {
-        console.log('listening on *:6969');
+    console.log('listening on *:6969');
 })
 
