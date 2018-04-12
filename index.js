992e72616b3d8cd3f0717ef199cbc6071403faee
James Lee <lee1870@purdue.edu>
2018-04-11 22:21:28 -0400

    Added createAccount and set headers and buttons for index

--- index.js
+++ index.js
@@ -9,24 +9,33 @@ var firebase = require('firebase');
 //var db = openDatabase('mydb', '4.0.0', 'my first database', 2 * 1024 * 1024);
 //create connection to database
 //if the database is NOT found, create one
-var db = new sqlite3.Database('Game-Plan users');
-db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS user (username TEXT, password TEXT, level INT)");
-
-});
+//var db = new sqlite3.Database('Game-Plan users');
+//var db = new firebase
+//db.serialize(function() {
+//    db.run("CREATE TABLE IF NOT EXISTS user (username TEXT, password TEXT, level INT)");
+//});
 /*
 function createDb(){
     console.log("creating Database
 
 }*/
 app.get('/', function(req, res){
-    res.send('<h1>Hello world<h1>');
+    res.sendFile(__dirname + '/index.html');
+    console.log("We've got a visitor!");
 });
 app.get('/createnewuser', function(req,res){
     res.send('<h1>Create New User Menu<h1>');
+    res.send('<h2>Please enter your credentials here!<h2>');
+//    res.send('<form action="/action_page.php">');
+//        res.send('First name: <input type="text" name="fname"><br>');
+//        res.send('Last name: <input type="text" name="lname"><br>');
+//        res.send('<input type="submit" value = "Submit">');
+//    res.send('</form>');
+    console.log("Entering create a new user!");
 });
 app.get('/play', function(req,res){
     res.send('<h1>Play game<h1>');
+    console.log("Someone wants to start a game");
 });
 http.listen(6969, function() {
     console.log('listening on *:6969');
