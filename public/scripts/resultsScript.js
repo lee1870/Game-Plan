c6dd86b13797c40a7498b9c751b644007cab05ff
Tyler Rex <rext@purdue.edu>
2018-04-26 06:59:34 -0400

    Reset to last working version

--- public/scripts/resultsScript.js
+++ public/scripts/resultsScript.js
@@ -1,15 +1,9 @@
 const nextQuestionButton = document.getElementById("nextQuestion");
 const goToAnswerQuestionButton = document.getElementById("goToAnswerQuestion");
-const goToIndexButton = document.getElementById("goToIndex");
-
-document.getElementById("numYes").innerHTML = localStorage.getItem("numYes");
+document.getElementById("numYes").innerHTML = localStorage.getItem("numYes") + " did press the button";
 
 document.getElementById("numNo").innerHTML = localStorage.getItem("numNo") + " did not press the button";
 
 nextQuestionButton.addEventListener("click", function() {
   window.location.href= "answerQuestion.html";
-});
-
-goToIndexButton.addEventListener("click", function() {
-  window.location.href= "index.html";
 });
