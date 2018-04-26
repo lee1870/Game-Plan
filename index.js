37964ccec536fca7ee5314fe7d735e1bf38780d2
James Lee <lee1870@purdue.edu>
2018-04-25 21:47:52 -0400

    Basic server functionality

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 //var sqlite3 = require('sqlite3').verbose();
 var firebase = require('firebase');
-var database = new Firebase("https://game-plan-4263.firebaseio.com");
+//var database = new Firebase("https://game-plan-4263.firebaseio.com");
 //var db = new sqlite3(':memory:');
 //var database = require('sqlite3.js');
 //var db = openDatabase('mydb', '4.0.0', 'my first database', 2 * 1024 * 1024);
@@ -24,9 +24,9 @@ function createDb(){
     console.log("creating Database
 
 }*/
-/*app.get('/', function(req, res){
+app.get('/', function(req, res){
     res.send('');
-});*/
+});
 /*app.get('/createnewuser', function(req,res){
     res.send('<h1>Create New User Menu<h1>');
 });
@@ -55,10 +55,4 @@ app.get('/play', function(req,res){
 http.listen(6969, function() {
     console.log('listening on *:6969');
 })*/
-
-
-var ref = firebase.database().ref();
-ref.on("value", function(snapshot){
-    output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
-});
 
---
+++ public/scripts/indexScript.js
@@ -0,0 +1,19 @@
+const goToCreateQuestionButton = document.getElementById("goToCreateQuestion");
+const goToPlayGameButton = document.getElementById("goToPlayGameButton");
+
+goToCreateQuestionButton.addEventListener("click", function() {
+  window.location.href= "createaccount.html";
+  
+});
+
+/*let src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js";
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAR2FdadMlQgqJAzKJF0mHZGniMMBMS5Rk",
+    authDomain: "game-plan-4263.firebaseapp.com",
+    databaseURL: "https://game-plan-4263.firebaseio.com",
+    projectId: "game-plan-4263",
+    storageBucket: "game-plan-4263.appspot.com",
+    messagingSenderId: "335501876781"
+  };
+  firebase.initializeApp(config);*/
