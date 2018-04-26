c2b4a93bff9892031617da15a19e17f1defa7014
Tyler Rex <rext@purdue.edu>
2018-04-26 06:56:01 -0400

    Finished all css

--- public/scripts/resultsScript.js
+++ public/scripts/resultsScript.js
@@ -1,8 +1,8 @@
 const nextQuestionButton = document.getElementById("nextQuestion");
 const goToAnswerQuestionButton = document.getElementById("goToAnswerQuestion");
-document.getElementById("numYes").innerHTML = localStorage.getItem("numYes");
+document.getElementById("numYes").innerHTML = localStorage.getItem("numYes") + " did press the button";
 
-document.getElementById("numNo").innerHTML = localStorage.getItem("numNo");
+document.getElementById("numNo").innerHTML = localStorage.getItem("numNo") + " did not press the button";
 
 nextQuestionButton.addEventListener("click", function() {
   window.location.href= "answerQuestion.html";
