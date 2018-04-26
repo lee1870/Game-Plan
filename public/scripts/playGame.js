85b1b16db3b7d9b0b597f97f035f2df2187066a6
James Lee <lee1870@purdue.edu>
2018-04-25 22:34:12 -0400

    Added playGame backend file

---
+++ public/scripts/playGame.js
@@ -0,0 +1,17 @@
+const clickTheButton = document.getElementById("clickButton");
+const dontClickTheButton = document.getElementById("dontClickButton");
+const postComment = document.getElementById("postComment");
+const commentField = document.getElementById("inputComment");
+//const goToPlayGameButton = document.getElementById("goToPlayGameButton");
+
+clickTheButton.addEventListener("click", function() {
+  window.location.href= "createQuestion.html";
+});
+
+dontClickTheButton.addEventListener("click", function() {
+  
+});
+
+postComment.addEventListener("click",function() {
+
+});
