8a9e2a9ed823ddcf441e7250d3929c096ed3fa77
lee1870 <lee1870@purdue.edu>
2018-04-26 04:42:02 -0400

    Added thanks and fixed result broken page

---
+++ public/scripts/thanks.js
@@ -0,0 +1,7 @@
+const backToIndexButton = document.getElementById("backToIndex");
+//const goToAnswerQuestionButton = document.getElementById("goToAnswerQuestion");
+
+backToIndexButton.addEventListener("click", function() {
+  window.location.href= "index.html";
+});
+
