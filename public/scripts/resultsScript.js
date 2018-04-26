65368f4b89c8ee85bb7dc9df5022bb3880d86663
lee1870 <lee1870@purdue.edu>
2018-04-26 06:11:51 -0400

    Added button functionality

--- public/scripts/resultsScript.js
+++ public/scripts/resultsScript.js
@@ -1,9 +1,15 @@
 const nextQuestionButton = document.getElementById("nextQuestion");
 const goToAnswerQuestionButton = document.getElementById("goToAnswerQuestion");
-document.getElementById("numYes").innerHTML = localStorage.getItem("numYes") + " did press the button";
+const goToIndexButton = document.getElementById("goToIndex");
+
+document.getElementById("numYes").innerHTML = localStorage.getItem("numYes");
 
 document.getElementById("numNo").innerHTML = localStorage.getItem("numNo") + " did not press the button";
 
 nextQuestionButton.addEventListener("click", function() {
   window.location.href= "answerQuestion.html";
+});
+
+goToIndexButton.addEventListener("click", function() {
+  window.location.href= "index.html";
 });
